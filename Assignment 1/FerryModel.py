30564bc62ae3571c7224820c2b7a08957919060d
herzogrh <rico.herzog@outlook.com>
2020-09-17 19:47:33 +0200

    TimeTable added

--- Assignment 1/FerryModel.py
+++ Assignment 1/FerryModel.py
@@ -1,7 +1,10 @@
 # Imports
-import salabim as sim
-import numpy as np
-import pandas as pd
+import salabim as sim, numpy as np, pandas as pd
+import time, sys
+
+#Time data
+CAR_NUMBERS = pd.read_csv("TimeTable.csv", sep=";")
+CAR_NUMBERS[CAR_NUMBERS["Time"]< 15].tail(1)
 
 # Variables
 SAILING_TIME = sim.Triangular(10,18,13)
@@ -11,20 +14,49 @@ SIM_TIME = 60*24 # Time in minutes
 LOADING_TIME = 1/6 # 10 seconds per car
 UNLOADING_TIME = 5/60 # 5 seconds per time
 WAITING_TIME_PREPAID = 0.5 # minutes
+PERCENTAGE_PREPAID = 0.2 
+
+REPLICATIONS = 10 # Number of experiment replications
 
 # Components
 class Car(sim.Component):
-    def __init__(self, length, *args, **kwargs):
+    def __init__(self, cartype, paid, location, *args, **kwargs):
         sim.Component.__init__(self, *args, **kwargs)
-        self.cartype = cartype
-        self.paid = paid
-    def process(self):  
+        self.cartype = cartype # either tourist or employee
+        self.paid = paid # true for prepaid, false for not prepaid
+        self.location = location # either mainland or island
 
+    def process(self):  
+        # Assign itself to the correct queue
+
+        #
+        yield self.hold(1)
+
+
+class CarGenerator(sim.Component):
+    def process(self):
+        while True:
+            #Get current time
+            CurrentCarNumbers = CAR_NUMBERS[CAR_NUMBERS["Time"]< (env.now()/60)].tail(1)
+
+            # Mainland ----------------------
+            ## Generate Cars based on time
+
+
+            #Generate 
+
+            Car(cartype = "tourist", paid = False, location="mainland")
+
+
+            yield self.hold(60)
 
 
 # Create the Environment
-env = sim.Environment()
-
-env.run(duration=SIM_TIME, time_unit='minutes')
-env.animate(True)
+env = sim.Environment(time_unit='minutes', trace= True)
 env.modelname("Canadian Ferries Simulation")
+CarGenerator()
+env.run(duration=SIM_TIME)
+print()
+
+
+#Comp
